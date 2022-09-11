c1fa367c425e07d00779389ba06134e32dc4e2cd
Advitiay Anand <advitiay.a@gmail.com>
2022-09-11 19:15:58 +0530

    Change note upload button colors

--- app/src/main/java/com/github/adizcode/cloudynotes/ui/view/core/subscreen/NoteUploadSubScreen.kt
+++ app/src/main/java/com/github/adizcode/cloudynotes/ui/view/core/subscreen/NoteUploadSubScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
@@ -15,7 +16,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Upload
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.github.adizcode.cloudynotes.ui.theme.Background
+import com.github.adizcode.cloudynotes.ui.theme.TextBlack
 import com.github.adizcode.cloudynotes.ui.view.common.CoreSubScreenHeading
 import com.github.adizcode.cloudynotes.ui.view.common.CustomTextField
 import com.github.adizcode.cloudynotes.ui.viewmodel.NotesViewModel
@@ -44,17 +48,35 @@ fun NoteUploadSubScreen(
 
             Spacer(modifier = Modifier.height(15.dp))
 
-            Button(modifier = Modifier.fillMaxWidth(), onClick = openFile) {
+            Button(
+                modifier = Modifier.fillMaxWidth(), onClick = openFile,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Background,
+                    contentColor = TextBlack,
+                ),
+            ) {
                 Icon(Icons.Filled.Upload, null)
             }
         }
 
         Row(modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Absolute.spacedBy(20.dp)) {
-            OutlinedButton(modifier = Modifier.weight(0.5f), onClick = goBack) {
+            OutlinedButton(
+                modifier = Modifier.weight(0.5f), onClick = goBack,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.White,
+                    contentColor = TextBlack,
+                ),
+            ) {
                 Text("Back")
             }
-            Button(modifier = Modifier.weight(0.5f), onClick = viewModel::uploadNoteToStorage) {
+            Button(
+                modifier = Modifier.weight(0.5f), onClick = viewModel::uploadNoteToStorage,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Background,
+                    contentColor = TextBlack,
+                ),
+            ) {
                 Text("Add Note")
             }
         }
